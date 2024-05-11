e89f8b835fe0ebd9d4c0671f0c1d66d379777e42
Peter Maliar <peter.maliar@tabsandspaces.agency>
2024-05-11 20:39:36 +0300

    Added max locales

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,25 @@
+import {useTranslation} from "next-i18next";
+import {GetServerSidePropsContext, GetStaticProps, GetStaticPropsContext} from "next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+
 const About = () => {
-    return (<p>About page</p>)
+    const {t} = useTranslation('dashboard')
+    return (<div><p>About page</p><p>{t("1st step")}</p></div>)
+}
+
+export const getStaticProps: GetStaticProps = async (context: GetServerSidePropsContext | GetStaticPropsContext) => {
+    const localeData = {
+        locale: context.locale.split("-").shift(),
+        defaultLocale: context.defaultLocale.split("-").shift(),
+    };
+    return {
+        props: {
+            ...(await serverSideTranslations(localeData.locale, [
+                "common",
+                "dashboard",
+            ])),
+        }
+    };
 }
 
 export default About;
