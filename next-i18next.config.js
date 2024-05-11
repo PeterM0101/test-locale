c694c8a37b6b60f969e05cc50f379c48fc7c3452
Peter Maliar <peter.maliar@tabsandspaces.agency>
2024-05-11 10:41:31 +0300

    Added some logic to appWithTranslation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  defaultNS: "common",
+  ns: ["common"],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'el']
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { appWithTranslation } from "next-i18next";
+import {appWithTranslation, UserConfig} from "next-i18next";
+import nextI18NextConfig from "../next-i18next.config.js";
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(App)
+export default appWithTranslation(App, {
+  ...(nextI18NextConfig as UserConfig),
+
+  localePath: (locale= 'en', namespace, missing) => {
+
+    const path = `./public/locales/${locale}/${namespace}.json`;
+    console.debug(
+        `[SC] app.appWithTranslation locale=${locale}${" ".repeat(
+            10 - locale.length
+        )} path=${path}`, namespace
+    );
+
+    return path;
+  },
+})
